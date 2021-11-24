53f9618de511333f2b7842de3720c2877a88afe8
Sandro-Sousa <sandro-souza-oliveira@hotmail.com>
2021-11-23 23:26:28 -0300

    Initial Commit

---
+++ cadastro/cadastro.cross/AppSettings.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace velesemail.cross
+{
+    public class AppSettings
+    {
+        private static AppSettings _appSettings;
+        public string _appSettingValue { get; set; }
+
+        public AppSettings(IConfiguration config, string Key)
+        {
+            this._appSettingValue = config.GetValue<string>(Key);
+        }
+        public static string GetConnectionString(string key)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            IConfigurationRoot configuration = builder.Build();
+
+            return configuration.GetConnectionString(key);
+        }
+    }
+}
