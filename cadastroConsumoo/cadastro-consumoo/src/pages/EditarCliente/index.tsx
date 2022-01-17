c2af53bd36fe9e71b3a710638393cb8292fdefe7
Sandro-Sousa <sandro-souza-oliveira@hotmail.com>
2022-01-17 00:34:53 -0300

    Manutencao

--- cadastroConsumoo/cadastro-consumoo/src/pages/EditarCliente/index.tsx
+++ cadastroConsumoo/cadastro-consumoo/src/pages/EditarCliente/index.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useHistory, useParams, Link } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { array } from "yup";
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { event } from "jquery";
 
 interface IParam {
   id:string;
@@ -12,8 +15,12 @@ type Inputs = {
   cpf: string,
 };
 
-
 const EditarCliente: React.FC = () => {
+
+  const [telefone, setTelefone] = useState([{
+    numero: "",
+    idCliente: "",
+  }]);
 
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
 
@@ -30,6 +37,24 @@ const EditarCliente: React.FC = () => {
     setCliente({ ...cliente, [e.target.name]: e.target.value });
   };
 
+
+  const onInputChangeTelefone = (index:number, e: React.ChangeEvent<HTMLInputElement>) => {
+    const values:Array<any> = [...telefone];
+    values[index][e.target.name] = e.target.value;
+    setTelefone(values);
+  };
+
+  // const handleServiceChange = (e: React.ChangeEvent<HTMLInputElement>, index:number) => {
+  //   const { name, value } = e.target;
+  //   const list:Array<any> = [...serviceList];
+  //   list[index][name] = value;
+  //   setServiceList(list);
+  // };
+
+   // const onSubmitTelefone = async () => {
+  //   await axios.post("https://localhost:5001/api/cadastro/v1/cadastroTelefone", telefone);
+  // };
+
   useEffect(() => {
     clienteGetById();
   }, []);
@@ -39,9 +64,21 @@ const EditarCliente: React.FC = () => {
     history.push("/");
   };
 
+  const onSubmitTelefone = async (e:any) => {
+    e.preventDefault();
+    await axios.post("https://localhost:5001/api/cadastro/v1/cadastrotelefone", telefone);
+    
+  };
+
   const clienteGetById = async () => {
     const result = await axios.get(`https://localhost:5001/api/cadastro/v1/clientegetbyid/${id}`);
     setCliente(result.data);
+  };
+
+
+  const handleServiceAdd = () => {
+    setTelefone([...telefone, {  numero: "",
+    idCliente: "" }]);
   };
   return (
     <div className="container">
@@ -86,7 +123,47 @@ const EditarCliente: React.FC = () => {
           </div>
           <br />
           <button className="btn btn-warning btn-block">Atualizar</button>
+          <br />
         </form>
+        <br />
+        <h4 className="text-center mb-4">Telefone</h4>
+        <form onSubmit={handleSubmit(onSubmitTelefone)}>
+        {telefone.map((telefoneService, index) =>(
+            <div key={index}>
+            <div className="form-group">
+             <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Insira seu Telefone"
+              name="numero"
+              value={telefoneService.numero}
+              onChange={e => onInputChangeTelefone(index,e)}
+              />
+              <br />
+              <input 
+                  type="text"
+                  readOnly
+                  className="form-control form-control-lg"
+                  name="idCliente"
+                  value={telefoneService.idCliente = id}
+                  onChange={(e) => onInputChangeTelefone(index,e)}
+                   />
+            </div>
+            <br />
+            {telefone.length - 1 === index && telefone.length < 4 && (
+                <button
+                  type="button"
+                  onClick={handleServiceAdd}
+                  className="btn btn-success"
+                >
+                  <span>+</span>
+                </button>
+              )}
+          </div>
+          ))}
+          <br />
+          <button className="btn btn-warning btn-block" onClick={onSubmitTelefone}>Adicionar Telefone</button>
+          </form>
       </div>
     </div>
   );
